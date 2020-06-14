2f74ddd18de5a89f3d5e058a02418c7be099a947
Matt Bachmann <bachmann.matt@gmail.com>
2020-06-13 22:31:03 -0400

    Make sure all the code is included and take the generated serviceworker and index out of the coverage measurements

---
+++ src/electronMain.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+const ipc = require("electron").ipcMain;
+const { fork } = require("child_process");
+
+const babbleWorker = fork("./work.js");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../build/index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  win.loadURL(startUrl);
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+ipc.on("hello", (event) => {
+  babbleWorker.once("message", (message) => {
+    event.sender.send("hello-reply", message);
+  });
+  babbleWorker.send("hello");
+});
